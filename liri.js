98b6b0a9ea7219376733f37d098d8dbd17937a3f
Nicole Wilsey Starr <Nicole.WilseyStarr@gmail.com>
2019-02-14 13:45:49 -0600

    spotify, bands, and movies all working

--- liri.js
+++ liri.js
@@ -2,8 +2,6 @@ require("dotenv").config();
 
 var keys = require("./keys.js");
 
-// var input = process.argv[3];
-
 //=============== If statement for input =============== 
 
 switch (process.argv[2]) {
@@ -12,6 +10,12 @@ switch (process.argv[2]) {
         break;
     case "spotify-this-song":
         song()
+        break;
+    case "concert-this":
+        bands()
+        break;
+    case "do-what-it-says":
+        doIt()
         break;
 };
 
@@ -28,6 +32,10 @@ function omdb() {
 
     var movieName = process.argv[3];
 
+    if (!process.argv[3]){
+        movieName = "Mr. Nobody";
+    }
+
     //Loop through to put a + instead of a space for the qurl
     for (var i = 4; i < nodeArgs.length; i++) {
         //template literal to grab the next indices and add a + for the qurl
@@ -36,8 +44,6 @@ function omdb() {
 
     //Constructing the queryurl 
     var qurl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-
-    console.log(qurl);
 
     axios.get(qurl).then(
         function (response) {
@@ -49,7 +55,7 @@ function omdb() {
             console.log("Language: " + response.data.Language);
             console.log("Release Year: " + response.data.Year);
             console.log("Plot: " + response.data.Plot);
-            console.log("Actors: " + response.data.Actors);
+            console.log("Actors: " + response.data.Actors + "\n");
         }
     );
 };
@@ -58,12 +64,18 @@ function omdb() {
 
 //=============== Start Spotify ===============
 
+//    * If no song is provided then your program will default to "The Sign" by Ace of Base.
+
 function song() {
     var Spotify = require('node-spotify-api');
 
     var spotify = new Spotify(keys.spotify);
 
     var songName = process.argv[3];
+
+    if (!process.argv[3]){
+        songName = "The Sign";
+    }
 
     spotify.search({ type: 'track', query: songName }, function (err, data) {
         if (err) {
@@ -72,30 +84,71 @@ function song() {
         console.log("Artist Name: " + data.tracks.items[0].album.artists[0].name);
         console.log("Song Name: " + data.tracks.items[0].name);
         console.log("Album Name: " + data.tracks.items[0].album.name);
-        console.log("Link to Song: " + data.tracks.items[0].external_urls.spotify);
+        console.log("Link to Song: " + data.tracks.items[0].external_urls.spotify + "\n");
     });
 };
-
-//    * If no song is provided then your program will default to "The Sign" by Ace of Base.
 
 //=============== End Spotify ===============
 
 //=============== Start Bands in Town API ===============
 
-//Concert this
-// * This will search the Bands in Town Artist Events API 
-// (`"https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"`) 
-// for an artist and render the following information about each event to the terminal:
+// * if no artist name need default
 
-// * Name of the venue
+function bands() {
+    var axios = require("axios");
+    var moment = require("moment");
 
-// * Venue location
+    var nodeArgs = process.argv;
 
-// * Date of the Event (use moment to format this as "MM/DD/YYYY")
+    var artistName = process.argv[3];
+
+    if (!process.argv[3]){
+        artistName = "Cardi B";
+    }
+
+    //Loop through to put a + instead of a space for the qurl
+    for (var i = 4; i < nodeArgs.length; i++) {
+        //template literal to grab the next indices and add a + for the qurl
+        artistName += `+${nodeArgs[i]}`
+    }
+
+    //constructing the queryurl
+    var qurl = "https://rest.bandsintown.com/artists/" + artistName + "/events?app_id=codingbootcamp";
+
+    console.log(qurl);
+
+    axios.get(qurl).then(
+        function (response) {
+            for (var i = 0; i < response.data.length; i++) {
+                console.log("Venue: " + response.data[i].venue.name);
+                console.log("Location: " + response.data[i].venue.city);
+                var date = moment((response.data[i].datetime)).format("MM/DD/YYYY");
+                console.log("Date: " + date + "\n");
+            }
+        }
+    );
+};
 
 //=============== End Bands in Town API ===============
 
 //=============== Start Do What It Says ===============
+
+function doIt() {
+    var fs = require('fs');
+        fs.readFile('random.txt', "utf8", function (err, data) {
+         if (err){
+             console.log("Error: " + error);
+         }
+         console.log(data);
+         var dataArr = data.split(",");
+
+         console.log(dataArr);
+
+        if (dataArr[0] === 'spotify-this-song'){
+            song(dataArr[1]);
+        }; 
+    });
+};
 
 // 4. `node liri.js do-what-it-says`
 
